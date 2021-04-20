c8c58d2968054f54768a013391f71b6fe9fc83bc
DanielPichette <daniel.f.pichette@gmail.com>
2021-04-20 10:05:54 -0500

    created welcome and rules methods within the Game class

--- game.py
+++ game.py
@@ -7,5 +7,21 @@ class Game:
         self.player2 = Player()
 
     # member methods
+    def welcome(self):
+        print('no more of that boring old rock paper scissors. lets take it up a notch with the new and improved ROCK PAPER SCISSORS SPOCK!')
+
+    def rules(self):
+        print('How to play:')
+        print('this updated version the the old classic game is pretty similar just with a few new gesture options to throw down.')
+        print('Rock crushes Scissors')
+        print('Scissors cuts Paper')
+        print('Paper covers Rock')
+        print('Rock crushes Lizard')
+        print('Lizard poisons Spock')
+        print('Spock smashes Scissors')
+        print('Scissors decapitates Lizard')
+        print('Lizard eats Paper')
+        print('Paper disproves Spock')
+        print('Spock vaporizes Rock')
 
     def player_selection(self):
--- player.py
+++ player.py
@@ -2,9 +2,10 @@ class Player:
     def __init__(self):
         self.gestures = ['rock', 'paper', 'scissors', 'lizard', 'spock']
         self.name = 'player'
-        self.chosen_gesture = 'selection'
+        self.chosen_gesture = self.select()
         self.score = 0
 
         # member methods
+
     def select(self):
         pass
